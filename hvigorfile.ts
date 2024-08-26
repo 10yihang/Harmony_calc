282ac6ff07bf6238e812cf361044b60810dc399a
yihang_01 <15700041128@163.com>
2024-08-26 21:27:50 +0800

    标准计算器编写完毕

---
+++ hvigorfile.ts
@@ -0,0 +1,3 @@
+// Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
+export { AppTasksForArkUIX } from '@ohos/hvigor-ohos-arkui-x-plugin';
+console.log(require.resolve('@ohos/hvigor'))
